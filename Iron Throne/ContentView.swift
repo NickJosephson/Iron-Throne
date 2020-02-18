e36618a2cb69854979a17bf71496be0f2977ed06
Nicholas Josephson <josephs6@myumanitoba.ca>
2020-02-18 17:56:33 -0600

    Initial Commit

---
+++ Iron Throne/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Iron Throne
+//
+//  Created by Nicholas Josephson on 2020-02-18.
+//  Copyright © 2020 Throne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
