703375e0ac61543e309b2a848dcdda99a8b9d09f
Nicholas Josephson <nickgjosephson@gmail.com>
2020-03-06 04:49:05 -0600

    Clean up, add touch bar support

--- Iron Throne/ContentView.swift
+++ Iron Throne/ContentView.swift
@@ -10,12 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject private var login = LoginManager.shared
-    @State private var scheme = "https"
-    @State private var host = "api-dev.findmythrone.com"
-    @State private var path = "/washrooms"
-    @State private var port = ""
-    @State private var query = ""
-    @State private var requestBody = ""
+    @State private var requestInfo = RequestInfo()
     @State private var output = ""
     private var loginController = LoginUIViewController()
     
@@ -23,145 +18,22 @@ struct ContentView: View {
         VStack() {
             Form {
                 HStack {
-                    TextField("Scheme", text: $scheme).frame(maxWidth: 50)
-                    TextField("Host", text: $host)
-                    TextField("Port", text: $port).frame(maxWidth: 50)
+                    TextField("Scheme", text: $requestInfo.scheme).frame(maxWidth: 50)
+                    TextField("Host", text: $requestInfo.host)
+                    TextField("Port", text: $requestInfo.port).frame(maxWidth: 50)
                 }
-                TextField("Path", text: $path)
-                TextField("Query", text: $query)
+                TextField("Path", text: $requestInfo.path)
+                TextField("Query", text: $requestInfo.query)
                 HStack {
                     TextField("Token", text: $login.accessToken)
-                    Button(action: {
-                        self.loginController.startLogin()
-                    }, label: { Text("Login") })
+                    Button(action: { self.loginController.startLogin() }, label: { Text("Login") })
                 }
-                TextField("Body", text: $requestBody)
+                TextField("Body", text: $requestInfo.body)
                 HStack {
-                    Button(action: {
-                        self.output = "Fetching..."
-
-                        var urlComponent = URLComponents()
-                        urlComponent.scheme = self.scheme
-                        urlComponent.host = self.host
-                        urlComponent.path = self.path
-                        urlComponent.query = self.query
-                        if let portNumber = Int(self.port), self.port != "" {
-                            urlComponent.port = portNumber
-                        }
-                        
-                        if let url = urlComponent.url {
-                            print(url)
-                            fetch(url: url) { data in
-                                DispatchQueue.main.async {
-                                    if let prettyJSON = data.prettyPrintedJSONString {
-                                        self.output = prettyJSON as String
-                                    } else {
-                                        self.output = String(data: data, encoding: .utf8)!
-                                    }
-                                }
-                            }
-                        } else {
-                            self.output = "Request not send: Invalid URL"
-                        }
-                    }, label: { Text("GET") })
-                    Button(action: {
-                        self.output = "Sending..."
-
-                        var urlComponent = URLComponents()
-                        urlComponent.scheme = self.scheme
-                        urlComponent.host = self.host
-                        urlComponent.path = self.path
-                        urlComponent.query = self.query
-                        if let portNumber = Int(self.port), self.port != "" {
-                            urlComponent.port = portNumber
-                        }
-                        
-                        if let url = urlComponent.url {
-                            print(url)
-                            var request = URLRequest(url: url)
-                            request.httpBody = self.requestBody.data(using: .utf8)
-                            request.httpMethod = "POST"
-                            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                            
-                            print(self.requestBody)
-                            
-                            performRequestWithAuthentication(with: request) { data in
-                                DispatchQueue.main.async {
-                                    if let prettyJSON = data.prettyPrintedJSONString {
-                                        self.output = prettyJSON as String
-                                    } else {
-                                        self.output = String(data: data, encoding: .utf8)!
-                                    }
-                                }
-                            }
-                        } else {
-                            self.output = "Request not send: Invalid URL"
-                        }
-                    }, label: { Text("POST") })
-                    Button(action: {
-                        self.output = "Sending..."
-
-                        var urlComponent = URLComponents()
-                        urlComponent.scheme = self.scheme
-                        urlComponent.host = self.host
-                        urlComponent.path = self.path
-                        urlComponent.query = self.query
-                        if let portNumber = Int(self.port), self.port != "" {
-                            urlComponent.port = portNumber
-                        }
-                        
-                        if let url = urlComponent.url {
-                            print(url)
-                            var request = URLRequest(url: url)
-                            request.httpBody = self.requestBody.data(using: .utf8)
-                            request.httpMethod = "PUT"
-                            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-                            performRequestWithAuthentication(with: request) { data in
-                                DispatchQueue.main.async {
-                                    if let prettyJSON = data.prettyPrintedJSONString {
-                                        self.output = prettyJSON as String
-                                    } else {
-                                        self.output = String(data: data, encoding: .utf8)!
-                                    }
-                                }
-                            }
-                        } else {
-                            self.output = "Request not send: Invalid URL"
-                        }
-                    }, label: { Text("PUT") })
-                    Button(action: {
-                        self.output = "Sending..."
-
-                        var urlComponent = URLComponents()
-                        urlComponent.scheme = self.scheme
-                        urlComponent.host = self.host
-                        urlComponent.path = self.path
-                        urlComponent.query = self.query
-                        if let portNumber = Int(self.port), self.port != "" {
-                            urlComponent.port = portNumber
-                        }
-                        
-                        if let url = urlComponent.url {
-                            print(url)
-                            var request = URLRequest(url: url)
-                            request.httpBody = self.requestBody.data(using: .utf8)
-                            request.httpMethod = "DELETE"
-                            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-                            performRequestWithAuthentication(with: request) { data in
-                                DispatchQueue.main.async {
-                                    if let prettyJSON = data.prettyPrintedJSONString {
-                                        self.output = prettyJSON as String
-                                    } else {
-                                        self.output = String(data: data, encoding: .utf8)!
-                                    }
-                                }
-                            }
-                        } else {
-                            self.output = "Request not send: Invalid URL"
-                        }
-                    }, label: { Text("DELETE") })
+                    Button(action: { self.performRequest(method: "GET") }, label: { Text("GET") })
+                    Button(action: { self.performRequest(method: "POST") }, label: { Text("POST") })
+                    Button(action: { self.performRequest(method: "PUT") }, label: { Text("PUT") })
+                    Button(action: { self.performRequest(method: "DELETE") }, label: { Text("DELETE") })
                 }
             }
 
@@ -181,9 +53,34 @@ struct ContentView: View {
         }
         .padding()
         .frame(minWidth: 500, minHeight: 400)
+        .touchBar(
+            TouchBar {
+                Button(action: { self.performRequest(method: "GET") }, label: { Text("GET") })
+                Button(action: { self.performRequest(method: "POST") }, label: { Text("POST") })
+                Button(action: { self.performRequest(method: "PUT") }, label: { Text("PUT") })
+                Button(action: { self.performRequest(method: "DELETE") }, label: { Text("DELETE") })
+            }
+        )
     }
+    
+    func performRequest(method: String) {
+        self.output = "Performing Request..."
+        if let request = self.requestInfo.formRequest(method: method) {
+            performRequestWithAuthentication(with: request) { data in
+                DispatchQueue.main.async {
+                    if let prettyJSON = data.prettyPrintedJSONString {
+                        self.output = prettyJSON as String
+                    } else {
+                        self.output = String(data: data, encoding: .utf8)!
+                    }
+                }
+            }
+        } else {
+            self.output = "Request not send: Invalid URL"
+        }
+    }
+    
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -192,11 +89,42 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 extension Data {
-    
     var prettyPrintedJSONString: NSString? { /// NSString gives us a nice sanitized debugDescription
         guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
         let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
         let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
         return prettyPrintedString
     }
+}
+
+struct RequestInfo {
+    var scheme = "https"
+    var host = "api-dev.findmythrone.com"
+    var port = ""
+    var path = "/washrooms"
+    var query = ""
+    var body = ""
+    
+    func formRequest(method: String) -> URLRequest? {
+        var request: URLRequest? = nil
+        
+        var urlComponent = URLComponents()
+        urlComponent.scheme = scheme
+        urlComponent.host = host
+        urlComponent.path = path
+        urlComponent.query = query
+        if let portNumber = Int(port), port != "" {
+            urlComponent.port = portNumber
+        }
+        
+        if let url = urlComponent.url {
+            request = URLRequest(url: url)
+            request!.httpBody = body.data(using: .utf8)
+            request!.httpMethod = method
+            request!.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+        
+        return request
+    }
+    
 }
